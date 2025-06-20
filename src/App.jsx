da2769afb4d96b35c4769356d00bb34119b6be13
Aashi Goel <aashi25goel@gmail.com>
2025-06-20 22:37:43 +0530

    local storage logic added

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,26 @@ import { AuthContext } from './context/AuthProvider';
 const App = () => {
 
   const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+
+  // useEffect(() => {
+  //   if(authData) {
+  //     const loggedInUser = localStorage.getItem("loggedInUser");
+  //     if (loggedInUser) {
+  //       setUser (loggedInUser.role);
+  //     }
+  //   }
+  // },[authData]);
+  
 
   const handleLogin = (email , password) => {
     if(email == 'admin@me.com' && password == '123') {
       setUser('admin');
-    } else if(email == 'user@me.com' && password == '123') {
+      localStorage.setItem("loggedInUser" , JSON.stringify({role : 'admin'}))
+
+    } else if(authData && authData.employees.find((e) => email == e.email && e.password == password)) {
       setUser('employee');
+      localStorage.setItem('loggedInUser' , JSON.stringify({role : 'employee'}))
     } 
     
     else{
@@ -25,13 +39,14 @@ const App = () => {
   //   handleLogin('user@me.com' , 123);
   // }, []);
 
-  const data = useContext(AuthContext);
-  console.log(data)
-
   return (
     <>
-    {!user ? <Login handleLogin= {handleLogin}/> : ''}
-    {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard/>}
+    {/* {!user ? <Login handleLogin= {handleLogin}/> : ''}
+    {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard/>} */}
+
+    {!user && <Login handleLogin={handleLogin} />}
+    {user === 'admin' && <AdminDashboard />}
+    {user === 'employee' && <EmployeeDashboard />}
     </>
   )
 }
