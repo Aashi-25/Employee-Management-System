674542deb77a3fb9140b77ffa9d5b9b79514f4a1
Aashi Goel <aashi25goel@gmail.com>
2025-06-22 09:21:37 +0530

    logout button made functional

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { setLocalStorage } from '../../utils/localStorage'
 
 const Header = ({data}) => {
-  console.log(data);
+  
+  const logOutUser = () => {
+    localStorage.setItem("loggedInUser" , '');
+    window.location.reload();
+  }
   return (
     <div className='flex items-end justify-between'>
         <h1 className='text-2xl font-medium'>Hello <br/> <span className='text-3xl font-semibold'>{data?.firstName || 'User'} 👋</span> </h1>
-        <button className='bg-red-600 text-white px-5 py-2 rounded-md text-lg font-medium'>Log Out</button>
+        <button onClick={logOutUser} className='bg-red-600 text-white px-5 py-2 rounded-md text-lg font-medium'>Log Out</button>
     </div>
   )
 }
