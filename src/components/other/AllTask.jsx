6d4409a33a11dfa5d3cb61bb8f89a70473a47c14
Aashi Goel <aashi25goel@gmail.com>
2025-06-22 12:39:35 +0530

    admin dashboard ui changed

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,45 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
+
+    const authData = useContext(AuthContext);
+    console.log(authData.employees);
     return (
-        <div id='allTask' className='bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto'>
+        <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
             <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-                <h2>Aashi</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
+                <h2 className='w-1/5 text-lg font-medium'>Employee Name</h2>
+                <h3 className='w-1/5 text-lg font-medium'>New Task</h3>
+                <h5 className='w-1/5 text-lg font-medium'>Active Task</h5>
+                <h5 className='w-1/5 font-medium text-lg'>Completed</h5>
+                <h5 className='w-1/5 text-lg  font-medium' >Failed</h5>
             </div>
 
-            <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
-                <h2>Aashi</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
+            <div id='allTask' className='h-[80%]'>
+                {authData.employees.map(function (elem) {
+                    return <div className='border-2 font-medium border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+                        <h2 className='w-1/5 text-lg font-medium text-white-600'>{elem.firstName}</h2>
+                        <h3 className='w-1/5 text-lg font-medium text-blue-600'>{elem.taskCounts.newTask}</h3>
+                        <h5 className='w-1/5 text-lg font-medium text-yellow-400'>{elem.taskCounts.active}</h5>
+                        <h5 className='w-1/5 font-medium text-lg text-white-600'>{elem.taskCounts.completed}</h5>
+
+                        <h5 className='w-1/5 text-lg  font-medium text-red-600'>{elem.taskCounts.failed}</h5>
+                    </div>
+                })}
             </div>
 
-            <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
-                <h2>Aashi</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-
-            <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
-                <h2>Aashi</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-
-                        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-                <h2>Aashi</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-
-            <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded'>
-                <h2>Aashi</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
         </div>
     )
 }
 
-export default AllTask
+export default AllTask;
